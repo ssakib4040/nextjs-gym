dabf7a0004a57b043a03b75e53881abee63945b1
Sadman Sakib <ssakib4040@gmail.com>
2023-05-27 00:13:20 +0600

    Padding fixed in header

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import background from "@/public/photo-1534438327276-14e5300c3a48.avif";
 export default function page() {
   return (
     <>
-      {/* // <!-- Navbar --> */}
-      <nav className="bg-yellow-500 text-white py-4 px-8">
-        <div className="container mx-auto flex items-center justify-between">
+      {/* <!-- Navbar --> */}
+      <nav className="bg-yellow-500 text-white py-4">
+        <div className="container mx-auto flex items-center justify-between lg:px-16">
           <h1 className="text-2xl font-bold">Gym Website</h1>
           <ul className="flex space-x-4">
             <li>
@@ -34,30 +34,14 @@ export default function page() {
         </div>
       </nav>
 
-      {/* // <!-- Hero Section --> */}
-      {/* <section className="bg-yellow-400 py-20">
-        <div className="container mx-auto text-center">
-          <h2 className="text-4xl font-bold mb-4">Welcome to Our Gym</h2>
-          <p className="text-xl text-gray-800">
-            Get fit and stay healthy with our professional trainers.
-          </p>
-          <a
-            href="#"
-            className="mt-8 inline-block bg-yellow-600 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-yellow-700"
-          >
-            Join Now
-          </a>
-        </div>
-      </section> */}
-
-      {/* // <!-- Hero Section --> */}
+      {/* <!-- Hero Section --> */}
       <section
         className="bg-yellow-500 bg-cover bg-center py-28"
         style={{
           backgroundImage: `url(${background.src})`,
         }}
       >
-        <div className="container mx-auto">
+        <div className="lg:px-16 mx-auto">
           <div className="text-yellow-500  text-center">
             <h1 className="text-5xl text-yellow-500  font-bold mb-4">
               Welcome to Our Gym
@@ -80,7 +64,7 @@ export default function page() {
       </section>
 
       {/* <!-- About Section --> */}
-      <section className="py-20 md:px-16">
+      <section className="py-20 lg:px-16">
         <div className="container mx-auto flex items-center">
           <div className="w-1/2">
             <h2 className="text-3xl font-bold mb-4">About Us</h2>
@@ -164,7 +148,7 @@ export default function page() {
       </section>
 
       {/* <!-- Footer --> */}
-      <footer className="bg-yellow-500 text-white py-8 px-8">
+      <footer className="bg-yellow-500 text-white py-8 lg:px-16">
         <div className="container mx-auto text-center">
           &copy; 2023 Gym Website. All rights reserved.
         </div>
